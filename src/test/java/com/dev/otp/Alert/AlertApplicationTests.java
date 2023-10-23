288bbac35b117eca3c80610b10df869a9b7cbdbb
Nagendra1160T <nagendrathoram66@gmail.com>
2023-10-23 17:12:34 +0530

    First commit after creating project

---
+++ src/test/java/com/dev/otp/Alert/AlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.otp.Alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
