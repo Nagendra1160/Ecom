1ec1d71df7473746e058f5f850e428293791413d
Nagendra1160T <nagendrathoram66@gmail.com>
2023-10-27 14:17:52 +0530

    Database connected but not inserting data

--- src/main/java/com/dev/otp/Alert/Model/UserEntity.java
+++ src/main/java/com/dev/otp/Alert/Model/UserEntity.java
@@ -1,11 +1,17 @@
 package com.dev.otp.Alert.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
+@Table(name="user_entity")
+
 public class UserEntity {
     @Id
+    @Column(name="email")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private String email;
 
 }
--- src/main/java/com/dev/otp/Alert/Service/UserService.java
+++ src/main/java/com/dev/otp/Alert/Service/UserService.java
@@ -5,11 +5,12 @@ import com.dev.otp.Alert.Repo.DBStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 
 @Service
-
+@Component
 public class UserService {
     @Autowired
     DBStore db;
@@ -20,7 +21,8 @@ public class UserService {
        }
        catch(Exception e)
        {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving user");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving user"+ e);
        }
 
     }
